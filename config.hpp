2256f4740b8ce34ae7a96c910bca71dc109e0192
mrdcvlsc <mrdcvlsc@gmail.com>
2023-02-19 00:06:25 +0800

    updated epi.hpp
    * Added predefined unsigned types for number class, eg. uint128_t, uint256_t, & etc.
    * Updated ostream operator of number class to prevent printing front zero limbs.
    * Added configuration header to find the best template type arguments for the predefined unsigned types of the number class.
    * Implemented the + and += operator for the number class.

---
+++ config.hpp
@@ -0,0 +1,12 @@
+#ifndef EXTENDED_PRECISION_INTEGERS_CONFIG_HPP
+#define EXTENDED_PRECISION_INTEGERS_CONFIG_HPP
+
+#if defined(__SIZEOF_INT128__) || defined(UINT128MAX)
+    #define ENV_64BIT_EXTENDED
+#elif defined(__amd__64__) || defined(__amd_64) || defined(__x86_64__) || defined(__x86_64))
+    #define ENV_64_BIT
+#elif defined(__INTEL__) || defined(__i386__) || defined(_M_IX86) || defined(__arm__)
+    #define ENV_32_BIT
+#endif
+
+#endif
--- epi.hpp
+++ epi.hpp
@@ -8,6 +8,7 @@
 #include <memory>
 #include <utility>
 #include <sstream>
+#include "config.hpp"
 
 namespace epi {
 
@@ -25,9 +26,19 @@ namespace epi {
         private:
             limb_t limbs[limb_n];
         public:
-            number() {}
 
-            constexpr number(std::initializer_list<limb_t> num) {
+            /// @brief Number base use to represent the integer.
+            constexpr static size_t limb_number_base = (sizeof(limb_t) * 8);
+
+            constexpr number() {
+                if constexpr (sizeof(limb_t) * 2 != sizeof(cast_t)) {
+                    throw std::invalid_argument(
+                        "In 'number<limb_t, cast_t, ...>' : the sizeof(cast_t) should be 2 times the sizeof(limb_t)."
+                    );
+                }
+            }
+
+            constexpr number(std::initializer_list<limb_t> num) : number() {
                 if (sizeof(limb_t) * limb_n < sizeof(limb_t) * num.size()) {
                     throw std::invalid_argument("initializer list has a bigger size than the defined number<> type");
                 }
@@ -44,8 +55,6 @@ namespace epi {
 
             template<typename T>
             constexpr number(const T& num) {
-                constexpr size_t number_size = sizeof(limb_t) * limb_n;
-
                 if constexpr (sizeof(T) <= sizeof(limb_t) * limb_n) {
                     std::memcpy(limbs, &num, sizeof(T));
                 } else {
@@ -54,26 +63,75 @@ namespace epi {
             }
 
             friend std::ostream &operator<< <limb_t, cast_t, limb_n>( std::ostream&, const number<limb_t, cast_t, limb_n>& );
+    
+            constexpr number operator+(number const& adder) const {
+                number sum;
+                limb_t carry = 0;
+
+                for (size_t i = 0; i < limb_n; ++i) {
+                    cast_t index_sum = (cast_t) limbs[i] + adder.limbs[i] + carry;
+                    sum.limbs[i] = index_sum;
+                    carry = index_sum >> limb_number_base;
+                }
+
+                return sum;
+            }
+
+            constexpr number &operator+=(number const& adder) {
+                limb_t carry = 0;
+
+                for (size_t i = 0; i < limb_n; ++i) {
+                    cast_t index_sum = (cast_t) limbs[i] + adder.limbs[i] + carry;
+                    limbs[i] = index_sum;
+                    carry = index_sum >> limb_number_base;
+                }
+
+                return *this;
+            }
     };
 
     template <typename limb_t, typename cast_t, size_t limb_n>
     std::ostream& operator<<( std::ostream& out, const number<limb_t, cast_t, limb_n>& num ) {
-
+        
         if (out.flags() & std::ios_base::hex) {
+            auto prev_ios_width = out.width();
+            auto prev_ios_fill  = out.fill();
             constexpr size_t padding = sizeof(limb_t) * 2;
             
             if constexpr (std::is_same<limb_t, char>::value || std::is_same<limb_t, unsigned char>::value) {
-                for (size_t i = 0; i < limb_n; ++i) {
+                size_t i = 0;
+                for (; i < limb_n; ++i) {
+                    if(num.limbs[limb_n - 1 - i]) {
+                        std::cout << (unsigned int) num.limbs[limb_n - 1 - i];
+                        i++;
+                        break;
+                    } else if (i == limb_n - 1 && !num.limbs[limb_n - 1 - i]) {
+                        std::cout << 0x0;
+                    }
+                }
+                
+                for (; i < limb_n; ++i) {
                     out << std::setfill('0') << std::setw(padding) << std::right
                         << (unsigned int) num.limbs[limb_n - 1 - i];
                 }
-                out << std::setfill(' ') << std::setw(0) << std::dec;
+                out << std::setfill(prev_ios_fill) << std::setw(prev_ios_width) << std::dec;
             } else {
-                for (size_t i = 0; i < limb_n; ++i) {
+                size_t i = 0;
+                for (; i < limb_n; ++i) {
+                    if(num.limbs[limb_n - 1 - i]) {
+                        std::cout << num.limbs[limb_n - 1 - i];
+                        i++;
+                        break;
+                    } else if (i == limb_n - 1 && !num.limbs[limb_n - 1 - i]) {
+                        std::cout << 0x0;
+                    }
+                }
+
+                for (; i < limb_n; ++i) {
                     out << std::setfill('0') << std::setw(padding) << std::right
                         << num.limbs[limb_n - 1 - i];
                 }
-                out << std::setfill(' ') << std::setw(0) << std::dec;
+                out << std::setfill(prev_ios_fill) << std::setw(prev_ios_width) << std::dec;
             }
         } else if (out.flags() & std::ios_base::oct) {
             // print oct
@@ -85,6 +143,35 @@ namespace epi {
         
         return out;
     }
+
+    // predefined types
+    #if defined(ENV_64BIT_EXTENDED)
+    typedef number<uint64_t, __uint128_t,  2>  uint128_t;
+    typedef number<uint64_t, __uint128_t,  3>  uint192_t;
+    typedef number<uint64_t, __uint128_t,  4>  uint256_t;
+    typedef number<uint64_t, __uint128_t,  5>  uint320_t;
+    typedef number<uint64_t, __uint128_t,  8>  uint512_t;
+    typedef number<uint64_t, __uint128_t, 16> uint1024_t;
+    typedef number<uint64_t, __uint128_t, 32> uint2048_t;
+    #elif defined(ENV_64BIT)
+    typedef number<uint32_t, uint64_t,  4>  uint128_t;
+    typedef number<uint32_t, uint64_t,  6>  uint192_t;
+    typedef number<uint32_t, uint64_t,  8>  uint256_t;
+    typedef number<uint32_t, uint64_t, 10>  uint320_t;
+    typedef number<uint32_t, uint64_t, 16>  uint512_t;
+    typedef number<uint32_t, uint64_t, 32> uint1024_t;
+    typedef number<uint32_t, uint64_t, 64> uint2048_t;
+    #elif defined(ENV_32BIT)
+    typedef number<uint16_t, uint32_t,   4>   uint64_t;
+    typedef number<uint16_t, uint32_t,   8>  uint128_t;
+    typedef number<uint16_t, uint32_t,  12>  uint192_t;
+    typedef number<uint16_t, uint32_t,  16>  uint256_t;
+    typedef number<uint16_t, uint32_t,  20>  uint320_t;
+    typedef number<uint16_t, uint32_t,  32>  uint512_t;
+    typedef number<uint16_t, uint32_t,  64> uint1024_t;
+    typedef number<uint16_t, uint32_t, 128> uint2048_t;
+    #endif
+
 } // namespace epi
 
 #endif
