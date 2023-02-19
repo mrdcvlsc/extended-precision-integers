71f78e2aa1922d292abe670cb3afbf3700f965ed
mrdcvlsc <mrdcvlsc@gmail.com>
2023-02-19 22:41:32 +0800

    added test cases for -, -= and *, *= operators

--- test.cpp
+++ test.cpp
@@ -35,6 +35,8 @@ void mem_compare(T1 a, T2 b, size_t numBytes) {
 int main() {
     epi::number<uint32_t, uint64_t, 7> test {0xff};
 
+    std::cout << "----------------------------------------------\n";
+    std::cout << "BASIC ADDITIONS : \n";
     {
         uint64_t a = 0xffffffffffffffff;
         uint64_t b = 0x1;
@@ -112,6 +114,182 @@ int main() {
         a_c += c_8t;
         a_a += a_8t;
         a_v += v_8t;
+
+        mem_compare(x, a_b, 8);
+        mem_compare(y, a_c, 8);
+        mem_compare(z, a_a, 8);
+        mem_compare(av, a_v, 8);
+    }
+
+    // #############################################################
+
+    std::cout << "\n----------------------------------------------\n";
+    std::cout << "BASIC SUBTRACTIONS\n";
+    {
+        uint64_t a = 0xffffffffffffffff;
+        uint64_t b = 0x1;
+        uint64_t c = 0xffffffff;
+        uint64_t x = a - b, y = a - c, z = a - a, m = b - c;
+        uint64_t v = 0xfffffffffffffff;
+        uint64_t av = a - v;
+
+        uint64_8t a_8t = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+        uint64_8t b_8t = { 0x1 };
+        uint64_8t c_8t = { 0xff, 0xff, 0xff, 0xff };
+        uint64_8t x_8t = a_8t - b_8t, y_8t = a_8t - c_8t, z_8t = a_8t - a_8t, m_8t = b_8t - c_8t;
+        uint64_8t v_8t = { 0xf, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+        uint64_8t av_8t = a_8t - v_8t;
+
+        mem_compare(a, a_8t, 8);
+        mem_compare(b, b_8t, 8);
+        mem_compare(c, c_8t, 8);
+
+        mem_compare(x, x_8t, 8);
+        mem_compare(y, y_8t, 8);
+        mem_compare(z, z_8t, 8);
+        mem_compare(m, m_8t, 8);
+        mem_compare(v, v_8t, 8);
+        mem_compare(av, av_8t, 8);
+
+        uint64_8t a_b = a_8t;
+        uint64_8t a_c = a_8t;
+        uint64_8t a_a = a_8t;
+        uint64_8t a_v = a_8t;
+
+        a_b -= b_8t;
+        a_c -= c_8t;
+        a_a -= a_8t;
+        a_v -= v_8t;
+
+        mem_compare(x, a_b, 8);
+        mem_compare(y, a_c, 8);
+        mem_compare(z, a_a, 8);
+        mem_compare(av, a_v, 8);
+    }
+
+    {
+        uint64_t a = 0xffffffffffffffff;
+        uint64_t b = 0x1;
+        uint64_t c = 0xffffffff;
+        uint64_t x = a - b, y = a - c, z = a - a, m = b - c;
+        uint64_t v = 0xfffffffffffffff;
+        uint64_t av = a - v;
+
+        uint64_16t a_8t = { 0xffff, 0xffff, 0xffff, 0xffff };
+        uint64_16t b_8t = { 0x1 };
+        uint64_16t c_8t = { 0xffff, 0xffff };
+        uint64_16t x_8t = a_8t - b_8t, y_8t = a_8t - c_8t, z_8t = a_8t - a_8t, m_8t = b_8t - c_8t;
+        uint64_16t v_8t = { 0xfff, 0xffff, 0xffff, 0xffff };
+        uint64_16t av_8t = a_8t - v_8t;
+
+        mem_compare(a, a_8t, 8);
+        mem_compare(b, b_8t, 8);
+        mem_compare(c, c_8t, 8);
+
+        mem_compare(x, x_8t, 8);
+        mem_compare(y, y_8t, 8);
+        mem_compare(z, z_8t, 8);
+        mem_compare(m, m_8t, 8);
+        mem_compare(v, v_8t, 8);
+        mem_compare(av, av_8t, 8);
+
+        uint64_16t a_b = a_8t;
+        uint64_16t a_c = a_8t;
+        uint64_16t a_a = a_8t;
+        uint64_16t a_v = a_8t;
+
+        a_b -= b_8t;
+        a_c -= c_8t;
+        a_a -= a_8t;
+        a_v -= v_8t;
+
+        mem_compare(x, a_b, 8);
+        mem_compare(y, a_c, 8);
+        mem_compare(z, a_a, 8);
+        mem_compare(av, a_v, 8);
+    }
+
+    // #############################################################
+
+    std::cout << "\n----------------------------------------------\n";
+    std::cout << "BASIC MULTIPLICATION\n";
+    {
+        uint64_t a = 0xffffffffffffffff;
+        uint64_t b = 0x1;
+        uint64_t c = 0xffffffff;
+        uint64_t x = a * b, y = a * c, z = a * a, m = b * c;
+        uint64_t v = 0xfffffffffffffff;
+        uint64_t av = a * v;
+
+        uint64_8t a_8t = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+        uint64_8t b_8t = { 0x1 };
+        uint64_8t c_8t = { 0xff, 0xff, 0xff, 0xff };
+        uint64_8t x_8t = a_8t * b_8t, y_8t = a_8t * c_8t, z_8t = a_8t * a_8t, m_8t = b_8t * c_8t;
+        uint64_8t v_8t = { 0xf, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+        uint64_8t av_8t = a_8t * v_8t;
+
+        mem_compare(a, a_8t, 8);
+        mem_compare(b, b_8t, 8);
+        mem_compare(c, c_8t, 8);
+
+        mem_compare(x, x_8t, 8);
+        mem_compare(y, y_8t, 8);
+        mem_compare(z, z_8t, 8);
+        mem_compare(m, m_8t, 8);
+        mem_compare(v, v_8t, 8);
+        mem_compare(av, av_8t, 8);
+
+        uint64_8t a_b = a_8t;
+        uint64_8t a_c = a_8t;
+        uint64_8t a_a = a_8t;
+        uint64_8t a_v = a_8t;
+
+        a_b *= b_8t;
+        a_c *= c_8t;
+        a_a *= a_8t;
+        a_v *= v_8t;
+
+        mem_compare(x, a_b, 8);
+        mem_compare(y, a_c, 8);
+        mem_compare(z, a_a, 8);
+        mem_compare(av, a_v, 8);
+    }
+
+    {
+        uint64_t a = 0xffffffffffffffff;
+        uint64_t b = 0x1;
+        uint64_t c = 0xffffffff;
+        uint64_t x = a * b, y = a * c, z = a * a, m = b * c;
+        uint64_t v = 0xfffffffffffffff;
+        uint64_t av = a * v;
+
+        uint64_16t a_8t = { 0xffff, 0xffff, 0xffff, 0xffff };
+        uint64_16t b_8t = { 0x1 };
+        uint64_16t c_8t = { 0xffff, 0xffff };
+        uint64_16t x_8t = a_8t * b_8t, y_8t = a_8t * c_8t, z_8t = a_8t * a_8t, m_8t = b_8t * c_8t;
+        uint64_16t v_8t = { 0xfff, 0xffff, 0xffff, 0xffff };
+        uint64_16t av_8t = a_8t * v_8t;
+
+        mem_compare(a, a_8t, 8);
+        mem_compare(b, b_8t, 8);
+        mem_compare(c, c_8t, 8);
+
+        mem_compare(x, x_8t, 8);
+        mem_compare(y, y_8t, 8);
+        mem_compare(z, z_8t, 8);
+        mem_compare(m, m_8t, 8);
+        mem_compare(v, v_8t, 8);
+        mem_compare(av, av_8t, 8);
+
+        uint64_16t a_b = a_8t;
+        uint64_16t a_c = a_8t;
+        uint64_16t a_a = a_8t;
+        uint64_16t a_v = a_8t;
+
+        a_b *= b_8t;
+        a_c *= c_8t;
+        a_a *= a_8t;
+        a_v *= v_8t;
 
         mem_compare(x, a_b, 8);
         mem_compare(y, a_c, 8);
